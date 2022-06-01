3580fbbf7f45d324d765f252c7d3a0a30014cb75
phuong-ta <phuong-ta@users.noreply.github.com>
2022-06-01 09:01:47 +0300

    create wellcome

---
+++ Bank.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int choice;
+    printf("\t Wellcome to PP Bank\n");
+    printf("1. hello\n");
+    printf("2. hello\n");
+    printf("3. hello\n");
+    printf("4. hello\n");
+    printf("5. hello\n");
+    printf("6. hello\n");
+    printf("7. Quit\n");
+    scanf("%d", choice);
+    
+    return 0;
+}
