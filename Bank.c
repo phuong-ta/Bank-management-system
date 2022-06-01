c72c07fff0040254c10e6c020328705c01e18d8a
phuong-ta <phuong-ta@users.noreply.github.com>
2022-06-01 21:03:33 +0300

    created switch cases

--- Bank.c
+++ Bank.c
@@ -3,14 +3,38 @@
 int main(){
     int choice;
     printf("\t Wellcome to PP Bank\n");
-    printf("1. hello\n");
-    printf("2. hello\n");
-    printf("3. hello\n");
-    printf("4. hello\n");
-    printf("5. hello\n");
-    printf("6. hello\n");
-    printf("7. Quit\n");
-    scanf("%d", choice);
-    
+    printf("1. create new account\n");
+    printf("2. update information\n");
+    printf("3. check anccout\n");
+    printf("4. transaction\n");
+    printf("5. remove account\n");
+    printf("6. quit\n");
+    printf("\n");
+    printf("Enter your choice\n");
+    scanf("%d", &choice);
+
+    switch (choice){
+    case 1:
+        printf("1. create new account\n");
+        break;
+    case 2:
+        printf("2. update information\n");
+        break;
+    case 3:
+        printf("3. check anccout\n");
+        break;
+    case 4:
+        printf("4. transaction\n");
+        break;
+    case 5:
+        printf("5. remove account\n");
+        break;
+    case 6:
+        printf("Thank you, see you again!\n");
+        break;
+    default:
+        break;
+    }
+
     return 0;
 }
